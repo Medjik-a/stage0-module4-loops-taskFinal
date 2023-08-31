e4c14103cafa5e1449cc9173481e30daa2406079
medjik <med.akhmetov@gmail.com>
2023-08-31 10:32:47 +0600

    first attempt

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for(int i=1;i<=cathetusLength;i++){
+            for(int j=cathetusLength;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int k=2;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
 
     }
 
